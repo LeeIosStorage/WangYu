1ddd9ac07773832f06611c3ceecc226ebf03f6d4
KID <147860548@qq.com>
2015-05-04 10:50:32 +0800

    整理

---
+++ WangYu/ViewControllers/Login/NewIntroViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewIntroViewController.h
+//  WangYu
+//
+//  Created by KID on 15/5/4.
+//  Copyright (c) 2015年 KID. All rights reserved.
+//
+
+#import "WYSuperViewController.h"
+
+@interface NewIntroViewController : WYSuperViewController
+
+@end
---
+++ WangYu/ViewControllers/Login/NewIntroViewController.m
@@ -0,0 +1,37 @@
+//
+//  NewIntroViewController.m
+//  WangYu
+//
+//  Created by KID on 15/5/4.
+//  Copyright (c) 2015年 KID. All rights reserved.
+//
+
+#import "NewIntroViewController.h"
+
+@interface NewIntroViewController ()
+
+@end
+
+@implementation NewIntroViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
