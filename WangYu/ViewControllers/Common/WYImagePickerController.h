ba7b0ee57a8e76aeead998286546d98fd314f5e9
Leejun <823974987@qq.com>
2015-05-26 18:53:13 +0800

    我的

---
+++ WangYu/ViewControllers/Common/WYImagePickerController.h
@@ -0,0 +1,13 @@
+//
+//  WYImagePickerController.h
+//  WangYu
+//
+//  Created by KID on 15/5/26.
+//  Copyright (c) 2015年 KID. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WYImagePickerController : UIImagePickerController
+
+@end
---
+++ WangYu/ViewControllers/Mine/PersonalEditViewController.h
@@ -0,0 +1,13 @@
+//
+//  PersonalEditViewController.h
+//  WangYu
+//
+//  Created by KID on 15/5/26.
+//  Copyright (c) 2015年 KID. All rights reserved.
+//
+
+#import "WYSuperViewController.h"
+
+@interface PersonalEditViewController : WYSuperViewController
+
+@end
