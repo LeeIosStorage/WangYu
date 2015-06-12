ab031bab99f177542e7751ddca468df2a3613fda
Leejun <823974987@qq.com>
2015-06-12 15:03:47 +0800

    优化

--- WangYu/ViewControllers/Mine/OrdersViewController.m
+++ WangYu/ViewControllers/Mine/OrdersViewController.m
@@ -699,15 +699,15 @@
         }
         [WYProgressHUD AlertSuccess:@"删除订单成功" At:weakSelf.view];
         orderInfo.isValid = 0;
-        [weakSelf.payOrderTableView reloadData];
+//        [weakSelf.payOrderTableView reloadData];
         
-//        NSInteger index = [weakSelf.payOrderList indexOfObject:orderInfo];
-//        if (index == NSNotFound || index < 0 || index >= weakSelf.payOrderList.count) {
-//            return;
-//        }
-//        NSIndexPath *indexPath = [NSIndexPath indexPathForRow:index inSection:0];
-//        [weakSelf.payOrderList removeObjectAtIndex:indexPath.row];
-//        [weakSelf.payOrderTableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+        NSInteger index = [weakSelf.payOrderList indexOfObject:orderInfo];
+        if (index == NSNotFound || index < 0 || index >= weakSelf.payOrderList.count) {
+            return;
+        }
+        NSIndexPath *indexPath = [NSIndexPath indexPathForRow:index inSection:0];
+        [weakSelf.payOrderList removeObjectAtIndex:indexPath.row];
+        [weakSelf.payOrderTableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
         
     } tag:tag];
 }
