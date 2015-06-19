66c4dd938e56355c35ff46739c640b171ff044a8
Leejun <823974987@qq.com>
2015-06-19 18:13:40 +0800

    个人信息界面

---
+++ WangYu/ViewControllers/Mine/PersonalProfileViewController.h
@@ -0,0 +1,16 @@
+//
+//  PersonalProfileViewController.h
+//  WangYu
+//
+//  Created by Leejun on 15/6/19.
+//  Copyright (c) 2015年 KID. All rights reserved.
+//
+
+#import "WYSuperViewController.h"
+#import "WYUserInfo.h"
+
+@interface PersonalProfileViewController : WYSuperViewController
+
+@property (nonatomic, strong) WYUserInfo *userInfo;
+
+@end
